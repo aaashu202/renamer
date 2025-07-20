ea26f0309b4282291be9ff31b3ae861b02c8706e
Mythic-botz <priyanshuprasad853@gmail.com>
2025-07-20 12:54:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 
 
-bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
+bot = Client(None, bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
 
 
 
