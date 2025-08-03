0df27bfbd23c41e40a38e4e5458e66a41f6351a4
aaashu202 <167896518+aaashu202@users.noreply.github.com>
2025-08-03 17:24:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,17 +20,16 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://telegram.me/TechifyBots'>Techify Bots</a>
-<b>🧑‍💻 Developer :</b> <a href='https://telegram.me/CallOwnerBot'>Rahul</a>
+<b>📢 Channel :</b> <a href='https://telegram.me/Dramafilez'>Dramafilez</a>
 
-<b>♻️ Bot Made By :</b> @TechifyBots"""
+<b>♻️ Bot Made By :</b> Zer0"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
 
 If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
 
-<b>🛍 UPI ID:</b> <code>TechifyBots@UPI</code> 
+<b>🛍 UPI ID:</b> <code></code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
@@ -47,6 +46,6 @@ If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upt
 
     METADATA_TXT = """<b><u>🖼️  HOW TO SET CUSTOM METADATA</u></b>
 For Example :-
-<code>By @TechifyBots</code>
-💬 For Any Help Contact @TechifySupport
+<code>By @Dramafilez</code>
+💬 For Any Help Contact @Dramafilez
 """
